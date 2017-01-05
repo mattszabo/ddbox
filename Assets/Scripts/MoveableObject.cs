a172e787878f4f215f2469aa469125482c6c8cc8
Matt Szabo <szabo.matthew@gmail.com>
2017-01-06 10:43:37 +1100

    code cleanup and import test tools

--- Assets/Scripts/MoveableObject.cs
+++ Assets/Scripts/MoveableObject.cs
@@ -10,6 +10,7 @@ public class MoveableObject : MonoBehaviour {
 	private bool isActive;
 	private bool isThrown;
 	private bool isHighlighted;
+
 	private GameObject laser;
 	private Vector3 objectLastPosition;
 	private Vector3 objectVelocity;
@@ -26,29 +27,19 @@ public class MoveableObject : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (isActive) {
-			Ray ray = new Ray (laser.transform.position - new Vector3(-.25f,.5f,0f), laser.transform.forward);
-			transform.position = ray.GetPoint (6.5f);
+			FollowPointer ();
+			CalculateObjectVelocity ();
 		}
 		if (isThrown) {
-			Debug.Log ("Thrown");
-			if (transform.position.y <= 0.5) {
-				isThrown = false;
-			} else {
-				Rigidbody rb = GetComponent<Rigidbody> ();
-				rb.AddForce (objectVelocity.x, objectVelocity.y, objectVelocity.z, ForceMode.Force);
-			}
+			ThrowObject ();
 		}
 		if (GvrController.ClickButtonDown && isHighlighted) {
 			ActivateObject();
 		}
 		if (GvrController.ClickButtonUp) {
 			InactivateObject ();
-			if (transform.position.y > 0.5) {
-				isThrown = true;
-			}
+			CheckForThrownObject ();
 		}
-		objectVelocity = (transform.position - objectLastPosition) / Time.fixedDeltaTime;
-		objectLastPosition = transform.position;
 	}
 
 	public void HighlightObject() {
@@ -65,6 +56,29 @@ public class MoveableObject : MonoBehaviour {
 		setColourToInactive ();
 		isHighlighted = false;
 		isActive = false;
+	}
+
+	public void FollowPointer() {
+		Ray ray = new Ray (laser.transform.position - new Vector3(-.25f,.5f,0f), laser.transform.forward);
+		transform.position = ray.GetPoint (6.5f);
+	}
+
+	public void CheckForThrownObject() {
+		if (transform.position.y > 0.5) {
+			isThrown = true;
+		}
+	}
+
+	public void CalculateObjectVelocity() {
+		objectVelocity = (transform.position - objectLastPosition) / Time.fixedDeltaTime;
+		objectLastPosition = transform.position;
+	}
+
+	public void ThrowObject() {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.AddForce (objectVelocity.x, objectVelocity.y, objectVelocity.z, ForceMode.Force);
+
+		isThrown = false;
 	}
 
 	public void setColorToHighlighted() {
